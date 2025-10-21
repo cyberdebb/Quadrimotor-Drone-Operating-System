c1b668ec86f96257248693b98382986275bd54c8
cyberdebb <deboramoreiracastro@gmail.com>
2025-10-21 17:01:37 -0300

    geração de sinal PWM

--- user_app.c
+++ user_app.c
@@ -3,16 +3,31 @@
 #include "user_app.h"
 #include "syscall.h"
 
+#define PWM_PERIOD      255 // 8-bit PWM period
+#define PWM_MIN_DUTY    0      
+#define PWM_MAX_DUTY    255    
+#define PWM_FREQ        50 // 50Hz PWM frequency
+
+// Add global variables for software PWM for all motors
+volatile uint8_t pwm_counter[4] = {0, 0, 0, 0}; // Counters for M1, M2, M3, M4
+volatile uint8_t duty_cycle[4] = {0, 0, 0, 0}; // Duty cycles for M1, M2, M3, M4
+
 void config_app(void) {
-    TRISCbits.RC2 = 0; // M1
-    TRISCbits.RC1 = 0; // M2
-    TRISDbits.RD0 = 0; // M3
-    TRISDbits.RD1 = 0; // M4
+    TRISCbits.RD0 = 0; // M1
+    TRISCbits.RD1 = 0; // M2
+    TRISDbits.RD2 = 0; // M3
+    TRISDbits.RD3 = 0; // M4
     TRISAbits.RA0 = 1; // gyroscope
     TRISAbits.RA1 = 1; // accelerometer
     TRISAbits.RA2 = 1; // batteries
 
     asm("GLOBAL _motors_control, _sensors_reading");
+
+    // Configure Timer0 for software PWM
+    T0CON = 0x88; // Enable Timer0, 8-bit, internal clock, 1:1 prescaler
+    TMR0IE = 1; // Enable Timer0 interrupt
+    PEIE = 1; // Enable peripheral interrupts
+    GIE = 1; // Enable global interrupts
 
     mutex_init(&mutex);
 }
@@ -37,6 +52,35 @@ unsigned int readADC(unsigned char canal) {
     return ((ADRESH << 8) + ADRESL); 
 }
 
+// Timer0 Interrupt Service Routine for software PWM
+void __interrupt() TMR0_ISR(void) {
+    if (TMR0IF) {
+        // Update all motors PWM
+        for(int i = 0; i < 4; i++) {
+            pwm_counter[i]++;
+            if (pwm_counter[i] >= PWM_PERIOD) {
+                pwm_counter[i] = 0;
+            }
+        }
+        
+        // Set motor pins based on PWM counters
+        PORTCbits.RC2 = (pwm_counter[0] < duty_cycle[0]) ? 1 : 0;  // M1
+        PORTCbits.RC1 = (pwm_counter[1] < duty_cycle[1]) ? 1 : 0;  // M2
+        PORTDbits.RD0 = (pwm_counter[2] < duty_cycle[2]) ? 1 : 0;  // M3
+        PORTDbits.RD1 = (pwm_counter[3] < duty_cycle[3]) ? 1 : 0;  // M4
+
+        TMR0IF = 0;  // Clear Timer0 interrupt flag
+    }
+}
+
+// Convert motor value (0.0 to 1.0) to duty cycle (0 to 255)
+uint8_t convert_to_duty_cycle(double motor_value) {
+    if (motor_value < 0.0) motor_value = 0.0;
+    if (motor_value > 1.0) motor_value = 1.0;
+    return (uint8_t)(motor_value * PWM_MAX_DUTY);
+}
+
+
 // Controls the velocities of each motor
 TASK motors_control(void) {
     while (1) {
@@ -44,12 +88,12 @@ TASK motors_control(void) {
         // ...
 
         if (mutex_lock(&mutex)) {
-            // Changes the motors velocities
-            PORTCbits.RC2 = motors_data.M1
-            PORTCbits.RC1 = motors_data.M2
-            PORTDbits.RD0 = motors_data.M3
-            PORTDbits.RD1 = motors_data.M4
-    
+            // Update all duty cycles
+            duty_cycle[0] = convert_to_duty_cycle(motors_data.M1);
+            duty_cycle[1] = convert_to_duty_cycle(motors_data.M2);
+            duty_cycle[2] = convert_to_duty_cycle(motors_data.M3);
+            duty_cycle[3] = convert_to_duty_cycle(motors_data.M4);
+            
             mutex_unlock(&mutex);
         }
 
