32c94131e293f8a7cc89e7cc9fe6f49f75f0e9b4
Daleffe <augustodaleffe@gmail.com>
2025-10-24 13:51:08 -0300

    add comments sobre API IO

--- io.c
+++ io.c
@@ -32,6 +32,7 @@ int adc_read()
     return ADRES;
 }
 
+// Inicializa o PWM com o período e prescaler especificados
 void pwm_init(uint8_t period, pwm_prescaler_t prescaler)
 {
     T2CONbits.TMR2ON = 0;
@@ -44,6 +45,7 @@ void pwm_init(uint8_t period, pwm_prescaler_t prescaler)
     TRISCbits.TRISC2 = 0;
 }
 
+// Inicia o PWM
 void pwm_start(void)
 {
     CCP1CONbits.CCP1M = 0b1100;
@@ -51,6 +53,7 @@ void pwm_start(void)
     T2CONbits.TMR2ON = 1;
 }
 
+// Para o PWM
 void pwm_stop(void)
 {
     T2CONbits.TMR2ON = 0;
@@ -58,6 +61,7 @@ void pwm_stop(void)
     LATCbits.LATC2 = 0;
 }
 
+// Define o ciclo de trabalho do PWM (0-1023)
 void pwm_set_duty(uint16_t duty)
 {
     if (duty > 1023) {
@@ -68,6 +72,7 @@ void pwm_set_duty(uint16_t duty)
     CCP1CONbits.DC1B = (uint8_t)(duty & 0x03);
 }
 
+// Inicializa a interrupção externa com a borda especificada
 void ext_int_init(ext_int_edge_t edge)
 {
     TRISBbits.TRISB0 = 1;
@@ -76,17 +81,20 @@ void ext_int_init(ext_int_edge_t edge)
     INTCONbits.INT0IF = 0;
 }
 
+// Habilita a interrupção externa
 void ext_int_enable(void)
 {
     INTCONbits.INT0IF = 0;
     INTCONbits.INT0IE = 1;
 }
 
+// Desabilita a interrupção externa
 void ext_int_disable(void)
 {
     INTCONbits.INT0IE = 0;
 }
 
+// Limpa a flag da interrupção externa
 void ext_int_clear_flag(void)
 {
     INTCONbits.INT0IF = 0;
